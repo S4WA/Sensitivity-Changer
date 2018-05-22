69451bd7b4bf9658e833854dc52a025bd377841d
Miyazawa <s4wa@outlook.com>
2018-05-23 03:55:07 +0900

    Add files via upload

--- 1.7.10/java/me/s4wa/SensChanger.java
+++ 1.7.10/java/me/s4wa/SensChanger.java
@@ -5,6 +5,14 @@ import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.lwjgl.input.Keyboard;
 
 import cpw.mods.fml.client.registry.ClientRegistry;
@@ -19,21 +27,20 @@ public class SensChanger
     public static final String MODID = "Sensitivity-changer";
     public static final String NAME = "Sensitivity Changer";
     public static final String VERSION = "1.0";
+    public static float delay = 0.00004F;
     private KeyBinding bindKeyUp;
     private KeyBinding bindKeyDown;
     private Minecraft mc = Minecraft.getMinecraft();
     private String text = null;
 
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
 		bindKeyUp = new KeyBinding("Up Sensitivity", Keyboard.KEY_UP, NAME);
 		bindKeyDown = new KeyBinding("Down Sensitivity", Keyboard.KEY_DOWN, NAME);
-		text = null;
 		MinecraftForge.EVENT_BUS.register(this);
-	    ClientRegistry.registerKeyBinding(bindKeyUp);
-	    ClientRegistry.registerKeyBinding(bindKeyDown);
-    }
+		ClientRegistry.registerKeyBinding(bindKeyUp);
+		ClientRegistry.registerKeyBinding(bindKeyDown);
+	}
 
 	@SubscribeEvent
 	public void onRender(RenderGameOverlayEvent event) {
@@ -41,20 +48,15 @@ public class SensChanger
 
 		if (Keyboard.isKeyDown(bindKeyUp.getKeyCode())) {
 			if (mc.gameSettings.mouseSensitivity >= 1) return;
-			mc.gameSettings.mouseSensitivity = mc.gameSettings.mouseSensitivity +0.000055F;
+			mc.gameSettings.mouseSensitivity = mc.gameSettings.mouseSensitivity +delay;
 		}
 		if (Keyboard.isKeyDown(bindKeyDown.getKeyCode())) {
 			if (mc.gameSettings.mouseSensitivity <= 0) return;
-			mc.gameSettings.mouseSensitivity = mc.gameSettings.mouseSensitivity -0.000055F;
+			mc.gameSettings.mouseSensitivity = mc.gameSettings.mouseSensitivity -delay;
 		}
 		if (Keyboard.isKeyDown(bindKeyUp.getKeyCode()) || Keyboard.isKeyDown(bindKeyDown.getKeyCode())) {
-			text = p();
+			text = ((int)(mc.gameSettings.mouseSensitivity * 200.0F)) + "%";
 			mc.fontRenderer.drawString(text, 10, 10, 0xFFFFFF);
 		}
-	}
-	
-	public String p() {
-		float sens = mc.gameSettings.mouseSensitivity;
-		return ((int)(sens * 200.0F)) + "%";
 	}
 }
